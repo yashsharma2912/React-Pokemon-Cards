658cfddf5fe4e0cbec9d33ae28c933493e8440c8
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-01-28 10:50:22 +0530

    project cards add need make them resposive for mobile device

--- pokemon/src/components/projcomp/card.jsx
+++ pokemon/src/components/projcomp/card.jsx
@@ -3,17 +3,26 @@ import '../projcomp/card.css'
 const Projectcard = (props) => {
   return (
     <div className="projectcards">
+      <div className="cardover">
+      </div>
         <div className="cardimg">
-            <img src="../src/assets/images/1.jpg" alt="project 1" className='pcimg' />
+            <img src={props.img} alt="project 1" className='pcimg' />
         </div>
         <div className="cardinfo">
            <h1>{props.name}</h1>
            <div className="infolink">
-           <h2>{props.date}<span><a target='_blank' href="https://github.com/yashsharma2912?tab=overview&from=2024-01-01&to=2024-01-26"><i class="ri-github-fill github sc"></i></a></span></h2>
-          
+           <h2>{props.date}</h2>
            </div>
            <p>{props.para}</p>
+           <div className="cardNav">
+           <a href={props.link1 }><i class={props.icon1}> </i></a>
+           <a href={props.link2 }><i class={props.icon2}> </i></a>
+           <a href={props.link3 }><i class={props.icon3}> </i></a>
+           <a href={props.link4 }><i class={props.icon4}> </i></a>
+           <a href={props.link5 }><i class={props.icon5}> </i></a>
+           </div>
         </div>
+        
     </div>
   )
 }
--- pokemon/src/components/projects.jsx
+++ pokemon/src/components/projects.jsx
@@ -7,10 +7,49 @@ const Projects = () => {
     <div className="Projects">
         <div className="topProject">
            <div className="projectgrid">
-             <Projectcard name="News-App-Using-Js" date="DEC 2023" para={ text1 }/>
-             <Projectcard name="Pinterest-Using-React" date="Jan 2024" para={ text1 }/>
-             <Projectcard name="News-App-Using-Js" date="DEC 2023" para={ text1 }/>
-           </div>
+
+             <Projectcard 
+
+              img = "../src/assets/images/1.jpg"
+              name="News-App-Using-Js"
+              date="DEC 2023" 
+              para={ text1 }
+             
+              icon1="ri-github-fill github sc"   
+              icon2="ri-javascript-fill sc"
+              icon3="ri-css3-fill sc "
+              icon4="ri-html5-fill sc"
+              icon5=" "
+
+              link1=" "
+              link2=" "
+              link3=" "
+              link4=" "
+              link5=" "
+              />
+
+              <Projectcard 
+
+              img = "../src/assets/images/1.jpg"
+              name="News-App-Using-Js"
+              date="DEC 2023" 
+              para={ text1 }
+             
+              icon1="ri-github-fill github sc"   
+              icon2="ri-javascript-fill sc"
+              icon3="ri-css3-fill sc "
+              icon4="ri-html5-fill sc"
+              icon5=" "
+
+              link1=" "
+              link2=" "
+              link3=" "
+              link4=" "
+              link5=" "
+              />
+
+
+             </div>
         </div>
         
     </div>
