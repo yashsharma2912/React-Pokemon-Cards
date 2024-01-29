d781fbcb8bce39dd3317850c1d2fc3557d790f46
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-01-29 09:47:03 +0530

    projects section done with fully responsiveness

--- pokemon/src/components/projects.jsx
+++ pokemon/src/components/projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
 
              <Projectcard 
 
-              img = "../src/assets/images/1.jpg"
+              img = "https://logodix.com/logo/571045.png"
               name="News-App-Using-Js"
               date="DEC 2023" 
               para={ text1 }
@@ -30,15 +30,36 @@ const Projects = () => {
 
               <Projectcard 
 
-              img = "../src/assets/images/1.jpg"
-              name="News-App-Using-Js"
+              img = "https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572-768x591.png"
+              name="BlogApp-Using-React"
               date="DEC 2023" 
               para={ text1 }
              
               icon1="ri-github-fill github sc"   
-              icon2="ri-javascript-fill sc"
-              icon3="ri-css3-fill sc "
+              icon2="ri-reactjs-line sc"
+              icon3="ri-vuejs-line sc "
               icon4="ri-html5-fill sc"
+              icon="ri-html5-fill sc"
+              icon5=" "
+
+              link1=" "
+              link2=" "
+              link3=" "
+              link4=" "
+              link5=" "
+              />
+              <Projectcard 
+
+              img = "http://pngimg.com/uploads/pinterest/pinterest_PNG62.png"
+              name="PinterestClone-Using-Mern"
+              date="DEC 2023" 
+              para={ text1 }
+             
+              icon1="ri-github-fill github sc"   
+              icon2="ri-reactjs-line sc"
+              icon3="ri-vuejs-line sc "
+              icon4="ri-html5-fill sc"
+              icon="ri-html5-fill sc"
               icon5=" "
 
               link1=" "
